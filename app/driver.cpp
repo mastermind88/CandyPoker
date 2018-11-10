46108c5dedf800d122df64b644b9652bffab3d28
Gerry Candy <gercan@hotmail.co.uk>
2018-11-10 13:17:05 +0000

    working on event_tree

--- app/driver.cpp
+++ app/driver.cpp
@@ -14,6 +14,7 @@
 #include "ps/base/algorithm.h"
 #include "ps/eval/instruction.h"
 #include "ps/base/rank_hasher.h"
+#include "ps/detail/tree_printer.h"
 #include "ps/base/suit_hasher.h"
 #include "ps/base/holdem_board_decl.h"
 #include "ps/eval/pass_mask_eval.h"
@@ -31,6 +32,7 @@
 #include <boost/intrusive/list.hpp>
 //#include <boost/program_options.hpp>
 #include "ps/eval/holdem_class_vector_cache.h"
+#include <boost/iterator/indirect_iterator.hpp>
 
 using namespace ps;
 
@@ -340,6 +342,31 @@ static TrivialCommandDecl<PrintRanks> PrintRanksDecl{"print-ranks"};
                         virtual void end_path(){}
                         virtual void player_fold(size_t player_idx){}
                         virtual void player_raise(size_t player_idx, double amt, bool allin){}
+                        virtual void post_sb(size_t player_idx, double amt, bool allin){}
+                        virtual void post_bb(size_t player_idx, double amt, bool allin){}
+                };
+                struct to_string_visitor : visitor{
+                        virtual void begin_path(){
+                                sstr_ << "begin_path();";
+                        }
+                        virtual void end_path(){
+                                sstr_ << "end_path();";
+                        }
+                        virtual void player_fold(size_t player_idx){
+                                sstr_ << "player_fold(" << player_idx << ");";
+                        }
+                        virtual void player_raise(size_t player_idx, double amt, bool allin){
+                                sstr_ << "player_raise(" << player_idx << "," << amt << "," << allin << ");";
+                        }
+                        virtual void post_sb(size_t player_idx, double amt, bool allin){
+                                sstr_ << "post_sb(" << player_idx << "," << amt << "," << allin << ");";
+                        }
+                        virtual void post_bb(size_t player_idx, double amt, bool allin){
+                                sstr_ << "post_bb(" << player_idx << "," << amt << "," << allin << ");";
+                        }
+                        std::string to_string()const{ return sstr_.str(); }
+                private:
+                        std::stringstream sstr_;
                 };
 
                 using strategy_impl_t = std::vector<Eigen::VectorXd>;
@@ -356,12 +383,39 @@ static TrivialCommandDecl<PrintRanks> PrintRanksDecl{"print-ranks"};
                                 return parent_->event_list() + "p";
                         return "";
                 }
+                virtual std::string to_string()const noexcept{ return "<>"; }
+
+                void display(){
+                        std::vector<std::vector<event_tree const*> > stack;
+                        stack.emplace_back();
+                        stack.back().push_back(this);
+                        
+                        for(;stack.size();){
+                                if( stack.back().empty()){
+                                        // A'
+                                        stack.pop_back();
+                                        continue;
+                                }
+                                auto head = stack.back().back();
+                                stack.back().pop_back();
+                                
+                                std::cout << std::string(stack.size()*2, ' ') << head->to_string() << "\n";
+                                if( ! head->is_terminal()){
+                                        stack.emplace_back();
+                                        for(size_t i=head->next_.size();i!=0;){
+                                                --i;
+                                                stack.back().push_back(head->next_[i].get());
+                                        }
+                                }
+                        }
+                }
 
                 
                 virtual double probability_of_event_given(strategy_impl_t const& S, holdem_class_vector const& cv)const noexcept{ return 1.0; }
                 virtual std::string event()const noexcept{ return ""; }
                 
-                static std::shared_ptr<event_tree> build(size_t n);
+                static std::shared_ptr<event_tree> build(size_t n, double sb, double bb, double eff);
+                static std::shared_ptr<event_tree> build_raise_fold(size_t n, double sb, double bb, double eff);
                 bool is_terminal()const{
                         return next_.empty();
                 }
@@ -372,13 +426,15 @@ static TrivialCommandDecl<PrintRanks> PrintRanksDecl{"print-ranks"};
 
                 virtual void apply(visitor& v)const{
                         std::vector<event_tree const*> path{this};
-                        for(; path.back()->parent_ != nullptr; ){
+                        for(;;){
+                                if( path.back()->parent_ == nullptr)
+                                        break;
                                 path.push_back(path.back()->parent_);
                         }
                         v.begin_path();
-                        for(size_t idx=0;idx!=path.size();){
+                        for(size_t idx=path.size();idx!=0;){
                                 --idx;
-                                apply_impl(v);
+                                path[idx]->apply_impl(v);
                         }
                         v.end_path();
                 }
@@ -391,7 +447,7 @@ static TrivialCommandDecl<PrintRanks> PrintRanksDecl{"print-ranks"};
                 }
                 void make_parent(){
                         std::vector<std::shared_ptr<event_tree>> stack = next_;
-                        for(;;){
+                        for(;stack.size();){
                                 auto head = stack.back();
                                 stack.pop_back();
                                 if( head->is_terminal() ){
@@ -406,18 +462,61 @@ static TrivialCommandDecl<PrintRanks> PrintRanksDecl{"print-ranks"};
 
                 terminals_vector_type terminals_;
         };
+
+        struct event_tree_hu_sb_bb : event_tree{
+                event_tree_hu_sb_bb(double sb, double bb)
+                        : sb_{sb}, bb_{bb}
+                {}
+        private:
+                virtual void apply_impl(visitor& v)const override{
+                        v.post_bb(0, bb_, false);
+                        v.post_sb(1, sb_, false);
+                }
+                double sb_;
+                double bb_;
+        };
+        struct event_tree_sb_bb : event_tree{
+                event_tree_sb_bb(double sb, double bb)
+                        : sb_{sb}, bb_{bb}
+                {}
+        private:
+                virtual void apply_impl(visitor& v)const override{
+                        v.post_sb(0, sb_, false);
+                        v.post_bb(1, bb_, false);
+                }
+                double sb_;
+                double bb_;
+        };
+
         struct event_tree_push : event_tree{
-                explicit event_tree_push(size_t player_idx, size_t index):player_idx_{player_idx}, index_{index}{}
+                explicit event_tree_push(size_t player_idx, size_t index, double amt):player_idx_{player_idx}, index_{index}, amt_{amt}{}
                 virtual double probability_of_event_given(strategy_impl_t const& S, holdem_class_vector const& cv)const noexcept override{
                         return S[index_][cv[player_idx_]];
                 }
                 virtual std::string event()const noexcept override{ return "p"; }
+                virtual std::string to_string()const noexcept override{ return "push"; }
         private:
                 virtual void apply_impl(visitor& v)const override{
-                        v.player_raise(player_idx_, amt_, true){}
+                        v.player_raise(player_idx_, amt_, true);
                 }
                 size_t player_idx_;
                 size_t index_;
+                double amt_;
+        };
+        struct event_tree_raise : event_tree{
+                explicit event_tree_raise(size_t player_idx, size_t index, double amt):player_idx_{player_idx}, index_{index}, amt_{amt} {}
+                virtual double probability_of_event_given(strategy_impl_t const& S, holdem_class_vector const& cv)const noexcept override{
+                        return S[index_][cv[player_idx_]];
+                }
+                virtual std::string event()const noexcept override{ return "r"; }
+                virtual std::string to_string()const noexcept override{ return "raise"; }
+        private:
+                virtual void apply_impl(visitor& v)const override{
+                        v.player_raise(player_idx_, amt_, false);
+                }
+                size_t player_idx_;
+                size_t index_;
+                double amt_;
         };
         struct event_tree_fold : event_tree{
                 explicit event_tree_fold(size_t player_idx, size_t index):player_idx_{player_idx}, index_{index}{}
@@ -425,6 +524,7 @@ static TrivialCommandDecl<PrintRanks> PrintRanksDecl{"print-ranks"};
                         return 1.0 - S[index_][cv[player_idx_]];
                 }
                 virtual std::string event()const noexcept override{ return "f"; }
+                virtual std::string to_string()const noexcept override{ return "fold"; }
         private:
                 virtual void apply_impl(visitor& v)const override{
                         v.player_fold(player_idx_);
@@ -432,35 +532,42 @@ static TrivialCommandDecl<PrintRanks> PrintRanksDecl{"print-ranks"};
                 size_t player_idx_;
                 size_t index_;
         };
-        struct event_tree_raise : event_tree{
-                explicit event_tree_raise(size_t player_idx, size_t index, double amt)
-                        :player_idx_{player_idx}, index_{index}, amt_{amt}
-                {}
-                virtual double probability_of_event_given(strategy_impl_t const& S, holdem_class_vector const& cv)const noexcept override{
-                        return S[index_][cv[player_idx_]];
-                }
-                virtual std::string event()const noexcept override{ return "r"; }
-        private:
-                virtual void apply_impl(visitor& v)const override{
-                        v.player_push(player_idx_);
-                }
-                size_t player_idx_;
-                size_t index_;
-                double amt_;
-        };
-        std::shared_ptr<event_tree> event_tree::build(size_t n){
-                auto root = std::make_shared<event_tree>();
+        std::shared_ptr<event_tree> event_tree::build_raise_fold(size_t n, double sb, double bb, double eff){
+                std::shared_ptr<event_tree> root;
+
+                root = std::make_shared<event_tree_hu_sb_bb>(sb, bb);
+                auto r = std::make_shared<event_tree_raise>(1, 0, 2);
+                auto f = std::make_shared<event_tree_fold>(1, 0);
+                root->add_child(r);
+                root->add_child(f);
+
+                auto rp = std::make_shared<event_tree_push>(0, 1, eff);
+                auto rf = std::make_shared<event_tree_fold>(0, 1);
+                r->add_child(rp);
+                r->add_child(rf);
+                
+                auto rpp = std::make_shared<event_tree_push>(1, 2, eff);
+                auto rpf = std::make_shared<event_tree_fold>(1, 2);
+                rp->add_child(rpp);
+                rp->add_child(rpf);
+
+                root->make_parent();
+                return root;
+        }
+        std::shared_ptr<event_tree> event_tree::build(size_t n, double sb, double bb, double eff){
+                std::shared_ptr<event_tree> root;
 
                 switch(n){
                         case 2:
                         {
-                                auto p = std::make_shared<event_tree_push>(0, 0);
-                                auto f = std::make_shared<event_tree_fold>(0, 0);
+                                root = std::make_shared<event_tree_hu_sb_bb>(sb, bb);
+                                auto p = std::make_shared<event_tree_push>(1, 0, eff);
+                                auto f = std::make_shared<event_tree_fold>(1, 0);
                                 root->add_child(p);
                                 root->add_child(f);
 
-                                auto pp = std::make_shared<event_tree_push>(1, 1);
-                                auto pf = std::make_shared<event_tree_fold>(1, 1);
+                                auto pp = std::make_shared<event_tree_push>(0, 1, eff);
+                                auto pf = std::make_shared<event_tree_fold>(0, 1);
                                 p->add_child(pp);
                                 p->add_child(pf);
 
@@ -484,33 +591,35 @@ static TrivialCommandDecl<PrintRanks> PrintRanksDecl{"print-ranks"};
                                              P F  P F   P F  
 
                                  */
-                                auto p = std::make_shared<event_tree_push>(0, 0);
-                                auto f = std::make_shared<event_tree_fold>(0, 0);
+                                root = std::make_shared<event_tree_sb_bb>(sb, bb);
+
+                                auto p = std::make_shared<event_tree_push>(2, 0, eff);
+                                auto f = std::make_shared<event_tree_fold>(2, 0);
                                 root->add_child(p);
                                 root->add_child(f);
 
-                                auto pp = std::make_shared<event_tree_push>(1, 1);
-                                auto pf = std::make_shared<event_tree_fold>(1, 1);
+                                auto pp = std::make_shared<event_tree_push>(0, 1, eff);
+                                auto pf = std::make_shared<event_tree_fold>(0, 1);
                                 p->add_child(pp);
                                 p->add_child(pf);
 
-                                auto fp = std::make_shared<event_tree_push>(1, 2);
-                                auto ff = std::make_shared<event_tree_fold>(1, 2);
+                                auto fp = std::make_shared<event_tree_push>(0, 2, eff);
+                                auto ff = std::make_shared<event_tree_fold>(0, 2);
                                 f->add_child(fp);
                                 f->add_child(ff);
                                 
-                                auto ppp = std::make_shared<event_tree_push>(2, 3);
-                                auto ppf = std::make_shared<event_tree_push>(2, 3);
+                                auto ppp = std::make_shared<event_tree_push>(1, 3, eff);
+                                auto ppf = std::make_shared<event_tree_fold>(1, 3);
                                 pp->add_child(ppp);
                                 pp->add_child(ppf);
                                 
-                                auto pfp = std::make_shared<event_tree_fold>(2, 4);
-                                auto pff = std::make_shared<event_tree_fold>(2, 4);
+                                auto pfp = std::make_shared<event_tree_push>(1, 4, eff);
+                                auto pff = std::make_shared<event_tree_fold>(1, 4);
                                 pf->add_child(pfp);
                                 pf->add_child(pff);
 
-                                auto fpp = std::make_shared<event_tree_fold>(2, 5);
-                                auto fpf = std::make_shared<event_tree_fold>(2, 5);
+                                auto fpp = std::make_shared<event_tree_push>(1, 5, eff);
+                                auto fpf = std::make_shared<event_tree_fold>(1, 5);
                                 fp->add_child(fpp);
                                 fp->add_child(fpf);
 
@@ -527,8 +636,14 @@ struct Scratch : Command{
         explicit
         Scratch(std::vector<std::string> const& args):args_{args}{}
         virtual int Execute()override{
-                auto gt = event_tree::build(2);
+                auto gt = event_tree::build_raise_fold(2, 0.5, 1.0, 10.0);
                 gt->display();
+
+                for(auto iter=gt->terminal_begin(),end=gt->terminal_end();iter!=end;++iter){
+                        event_tree::to_string_visitor v;
+                        iter->apply(v);
+                        std::cout << v.to_string() << "\n";
+                }
 
                 return EXIT_SUCCESS;
         }
